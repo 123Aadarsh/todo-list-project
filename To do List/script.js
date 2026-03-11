8c54e37377f1c5018753787fbe48e52318e341e4
123Aadarsh <aadarshdhiman17@gmail.com>
2026-03-11 20:33:14 +0530

    Add files via upload

---
+++ To do List/script.js
@@ -0,0 +1,60 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks(){
+localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks(){
+taskList.innerHTML = "";
+
+tasks.forEach((task, index) => {
+
+const li = document.createElement("li");
+
+li.innerHTML = `
+<span onclick="toggleTask(${index})" class="${task.completed ? 'completed' : ''}">
+${task.text}
+</span>
+
+<button class="delete-btn" onclick="deleteTask(${index})">X</button>
+`;
+
+taskList.appendChild(li);
+
+});
+
+}
+
+function addTask(){
+
+const text = taskInput.value.trim();
+
+if(text === "") return;
+
+tasks.push({
+text:text,
+completed:false
+});
+
+taskInput.value="";
+
+saveTasks();
+renderTasks();
+}
+
+function toggleTask(index){
+tasks[index].completed = !tasks[index].completed;
+saveTasks();
+renderTasks();
+}
+
+function deleteTask(index){
+tasks.splice(index,1);
+saveTasks();
+renderTasks();
+}
+
+renderTasks();
